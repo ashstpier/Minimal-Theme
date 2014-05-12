e0710e9d73fa67642454bd3e30c3e04f3ad97aba
Ashley St Pier <ashleystpier@gmail.com>
2014-05-12 18:44:04 +0100

    Init

--- index.php
+++ index.php
@@ -1,17 +1,46 @@
 <?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
+/** index.php
  *
- * @package WordPress
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @author		Ashley St Pier
+ * @package		The Bootstrap
+ * @since		1.0.0 - 05.02.2012
  */
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+get_header(); ?>
 
-/** Loads the WordPress Environment and Template */
-require('./wp-blog-header.php');
+<section id="primary" class="span8">
+	<?php tha_content_before(); ?>
+	<div id="content" role="main">
+		<?php tha_content_top();
+		
+		if ( have_posts() ) {
+			while ( have_posts() ) {
+				the_post();
+				get_template_part( '/partials/content', get_post_format() );
+			}
+			the_bootstrap_content_nav( 'nav-below' );
+		}
+		else {
+			get_template_part( '/partials/content', 'not-found' );
+		}
+	
+		tha_content_bottom(); ?>
+	</div><!-- #content -->
+	<?php tha_content_after(); ?>
+</section><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
+
+
+/* End of file index.php */
+/* Location: ./wp-content/themes/the-bootstrap/index.php */
